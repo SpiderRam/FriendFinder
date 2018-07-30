3b9b0cb65484974f228bd9c62c6b1407d42d4c3e
Carmen Owen <doctorsout@yahoo.com>
2018-07-30 18:32:01 -0400

    html and css started

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+const friendsArray = [
+    {
+        "name": "Malcolm",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/3/39/Mal.jpg/revision/latest?cb=20080301061839",
+        "scores": []
+    },
+    {
+        "name": "Zoe",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/1/10/Zoe.jpg/revision/latest?cb=20080301064448",
+        "scores": []
+    },
+    {
+        "name": "Wash",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/f/fa/Wash.jpg/revision/latest?cb=20080301062346",
+        "scores": []
+    },
+    {
+        "name": "Kaylee",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/5/55/Kaylee.jpg/revision/latest?cb=20100426173058",
+        "scores": []
+    },
+    {
+        "name": "Shepherd Book",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/e/e2/Book.jpg/revision/latest?cb=20080301063245",
+        "scores": []
+    },
+    {
+        "name": "Inara",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/c/c3/Inara02.jpg/revision/latest?cb=20080301063009",
+        "scores": []
+    },
+    {
+        "name": "Simon",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/6/63/Simon2.jpg/revision/latest?cb=20080308044029",
+        "scores": []
+    },
+    {
+        "name": "River",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/2/2c/River.jpg/revision/latest?cb=20080301023737",
+        "scores": []
+    },
+    {
+        "name": "Jayne",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/d/d0/Jayne.jpg/revision/latest?cb=20080301065044",
+        "scores": []
+    },
+    {
+        "name": "YoSaffBridge",
+        "photo": "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2015%2F01%2Ffirefly-christina-hendricks_320.jpg%3Fw%3D320&w=700&q=85",
+        "scores": []
+    },
+    {
+        "name": "Patience",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/1/16/Patience.jpg/revision/latest?cb=20080317010321",
+        "scores": []
+    },
+    {
+        "name": "Niska",
+        "photo": "https://vignette.wikia.nocookie.net/firefly/images/6/69/Niska.jpg/revision/latest?cb=20080307031032",
+        "scores": []   
+    }
+];
