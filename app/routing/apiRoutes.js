67ab9c84fbc7f9cd4328dce0d77085b5b472c2c4
Carmen Owen <doctorsout@yahoo.com>
2018-08-03 15:16:28 -0400

    jQuery and CSS working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
 var friends = require("../data/friends")
 
-
-
 module.exports = function(app) {
 
     app.get('/api/friends',function(req,res){
@@ -10,8 +8,24 @@ module.exports = function(app) {
   
 
     app.post('/api/new',function(req,res){
-        
-        
-    });
+
+        var compatibilityScore = 0;
+        var differences = [];
+        var currentUser = req.body;
+
+        for(var i = 0; i < friends.length; i++) {
+            
+            
+            for (var j = 0; j < currentUser.userScores.length; j++) {
+                var friendScore = friends.scores[i];
+                var userScore = currentUser.userScores[j];
+                var difference = friendScore - userScore;
+                var scoreDifference = Math.abs(difference);
+            };
+
+            differences.push(scoreDifference);
+        };
+
+ });
 
 };
