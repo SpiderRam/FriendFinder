b51da279b8e9ade1a12ab433cd71cd973ee7e230
Carmen Owen <doctorsout@yahoo.com>
2018-08-09 10:37:46 -0400

    math attempts

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,8 +2,6 @@ const friends = require("../data/friends");
 
 module.exports = function(app) {
 
-    var scoreDifference;
-
     app.get('/api/friends',function(req,res){
         res.json(friends);
     });
@@ -12,21 +10,34 @@ module.exports = function(app) {
     app.post('/api/new',function(req,res){
         console.log('Hello: ' + JSON.stringify(req.body));
         var compatibilityScore = 0;
+        var scoreDifference;
+        var friendScoreArray;
+        var userScoreArray;
+        var difference;
         var differences = [];
         var newUser = req.body;
 
         for(var i = 0; i < friends.length; i++) {
-            
-            
-            for (var j = 0; j < newUser.userScores.length; j++) {
-                var friendScore = friends.scores[i];
-                var userScore = newUser.scores[j];
-                var difference = friendScore - userScore;
-                scoreDifference = Math.abs(difference);
+        
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                friendScoreArray = friends[i].scores[j];
             }
-
-            differences.push(scoreDifference);
+            
         }
+
+        for (var k = 0; k < newUser.userScores.length; k++) {
+                
+            userScoreArray = newUser.scores[k];
+            
+        }
+
+        difference = friendScoreArray - userScoreArray;
+        scoreDifference = Math.abs(difference);
+        differences.push(scoreDifference);
+
+        compatibilityScore = differences.reduce(function(total, amount) {
+            return total + amount;
+          });
         
     });
  
