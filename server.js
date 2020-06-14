fb0339b1e3a6c962c30d86edaeea9317eaac12af
Carmen Owen <doctorsout@yahoo.com>
2020-06-14 10:55:49 -0400

    npm audit fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/app/public'));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function () {
+    console.log('App running at http://localhost:' + PORT + '/');
 });
